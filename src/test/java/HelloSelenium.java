3523990022c30c19e013f9697a42b9403447421f
jbioshock1912 <loh7457490>
2022-05-05 23:32:23 +0300

    testing: locators, buttons, dif. elements

---
+++ src/test/java/HelloSelenium.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class HelloSelenium extends  BaseUtils {
+
+    @Test
+    public void testFirstSelenium () {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://google.com");
+
+        driver.getTitle(); // => "Google"
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500)); //
+
+        WebElement searchBox = driver.findElement(By.name("q"));
+        WebElement searchButton = driver.findElement(By.name("btnK"));
+
+
+        searchBox.sendKeys("Selenium");
+        searchButton.click();
+
+        searchBox = driver.findElement(By.name("q"));
+        Assert.assertEquals(searchBox.getAttribute("value"), "Selenium"); // => "Selenium"
+
+        driver.quit();
+    }
+}
